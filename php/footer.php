0b13c1c0bdd8e27fc3277844307f88c854f1d439
Karl Smith <98028208+Ksmithprofessional@users.noreply.github.com>
2022-08-12 18:52:22 +0100

    Started adding php

---
+++ php/footer.php
@@ -0,0 +1,154 @@
+<?php
+
+?>
+
+    <!-- Email sign up form -->
+
+    <form action= "index.php" method="post">
+            <fieldset>
+                <legend>Email Newsletter Sign-Up</legend>
+                <span class="name">
+                    <label for="name"><span class="asterisk">Your Name</span><br>
+                        <input type="text" id="name" name="name" required>
+                    </label>
+                </span>
+
+                <span class="email">
+                    <label for="email"><span class="asterisk">Your Email</span><br>
+                        <input type="text" id="email" name="email" required>
+                    </label>
+                </span>
+
+                <br>
+                <input type="checkbox" id="marketing" name="marketing">
+                <label for="marketing">Please tick this box if you wish to receive marketing information from us. Please see our <a href="#">Privacy Policy</a> for more information on how we use your data. </label>
+                
+
+                <br>
+                <input type="submit" name= "submit" value="Subscribe">
+
+            </fieldset>
+        </form>
+
+        
+
+        <?php
+
+            include 'php/insert.php';
+        
+        ?>
+        
+
+    </main>
+
+    <!-- Footer with links and addresses/phone/ etc -->
+    <footer>
+
+    <!-- link lists in footer -->
+        <div class="footer">
+            <div class="footer-lists">
+
+                <div class="about-netmatters">
+                    <h3>About Netmatters </h3> 
+                        
+                        <p><a href="#">News</a></p>
+                        <p><a href="#">Our Careers </a></p>
+                        <p><a href="#">Our Team </a></p>
+                        <p><a href="#">Our Office Tour </a></p>
+                        <p><a href="#">Privacy Policy </a></p>
+                        <p><a href="#">Cookie Policy </a></p>
+                        <p><a href="#">Terms & Conditions </a></p>
+                        <p><a href="#">Environmental Policy </a></p>
+                    
+                </div>
+
+                <div class="our-services">
+                    <h3>Our Services </h3>
+                        
+                        <p><a href="#">Bespoke Software </a></p>
+                        <p><a href="#">IT Support  </a></p>
+                        <p><a href="#">Digital Marketing  </a></p>
+                        <p><a href="#">Telecoms Services  </a></p>
+                        <p><a href="#">Web Design  </a></p>
+                        <p><a href="#">Cyber Security  </a></p>
+                        <p><a href="#">Developer Training  </a></p>
+                        
+                </div>
+
+            </div>
+
+                <!-- Addresses in footer -->
+
+            <div class="footer-address">
+                <div>
+                    <h3>Cambridge Office </h3>
+                        <address>      
+                            Unit 1.28, <br>
+                            St John's Innovation Centre, <br>
+                            Cowley Road, Milton,<br>
+                            Cambridge, <br>
+                            CB4 0WS <br>
+                            <br>
+                            Tel:<a href="tel:#" class="tel"> 01223 37 57 72</a>
+                        </address>
+                </div>
+
+                <div class="address-2">
+                    <h3>Wymondham Office  </h3>
+                    <address>      
+                        Unit 15, <br>
+                        Penfold Drive, <br>
+                        Gateway 11 Business Park, <br>
+                        Wymondham, Norfolk, <br>
+                        NR18 0WZ <br>
+                        <br>
+                    Tel:<a href="tel:#" class="tel"> 01603 70 40 20</a>
+                    </address>
+                </div>
+
+                <div>
+                    <h3>Great Yarmouth Office </h3>
+                    <address>      
+                        Suite F23, <br>
+                        Beacon Innovation Centre, <br>
+                        Beacon Park, Gorleston, <br>
+                        Great Yarmouth, Norfolk, <br>
+                        NR31 7RA <br>
+                        <br>
+                    Tel:<a href="tel:#" class="tel"> 01493 60 32 04</a>
+                    </address>
+                </div>
+
+                </div>
+
+                <!-- <hr> -->
+                <div class="footnote">
+                    <p>&copy; Copyright Netmatters Ltd. 2022. All rights reserved.</p>
+                    <p>- <a href="#">Sitemap</a></p>
+                    <div class="social">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+            </div>
+        </div>
+
+    
+
+    </footer>
+
+    <!-- Partners part 2 -->
+    <div class="partner-background-fix">
+        <aside class="partner-2">
+        <img src="Images/silver-microsoft-partner.jpg" alt="Silver microsoft partner" class="silver-micro">
+        <img src="Images/future-50.jpg" alt="Future 50 member" class="future-50">
+        <img src="Images/qms.jpg" alt="QMS ISO registered" class="qms">
+        <img src="Images/norfolk-carbon-charter.jpg" alt="Silver Norfolk carbon charter 2019" class="silver-norfolk">
+        <img src="Images/investing-in-future-growth.jpg" alt="Investing in future growth winner" class="investing-future">
+        <img src="Images/princess-royal-training.jpg" alt="Princess royal training award" class="princess-royal">
+        <img src="Images/skills-of-tomorrow.jpg" alt="Skills of tomorrow winner" class="skills-of-tomorrow">
+        <a href="#"><img src="Images/netmatters-joins-the-5H8P.jpg" alt="Netmatters joins prompt payment code" class="payment-code"></a>
+        </aside>
+    </div>
+</div>
---
+++ php/header.php
@@ -0,0 +1,451 @@
+<?php
+
+?>
+
+<!-- pop out menu -->
+<div class="pop-out-menu menu-not-visible">
+    <a href="#"><div class="contact-top">Contact Us</div></a>
+
+    <div class="bespoke-menu">
+
+        <a href="#">
+            <div class="menu-title">
+            <i class="fas fa-th bespoke-icon"></i>
+            <p class="small-text">Bespoke</p>
+            <p class="large-text">Software</p>
+            </div>
+        </a>
+
+        <ul>
+            <li><a href="#">Workflow</a></li>
+            <li><a href="#">Automation</a></li>
+            <li><a href="#">Integration</a></li>
+            <li><a href="#">Database</a></li>
+            <li><a href="#">Sharepoint</a></li>
+            <li><a href="#">Management</a></li>
+            <li><a href="#">Navision</a></li>
+        </ul>
+    </div>
+
+    <div class="it-menu">
+
+        <a href="#">
+            <div class="menu-title">
+            <i class="fas fa-desktop it-icon"></i>
+            <p class="small-text">IT</p>
+            <p class="large-text">Support</p>
+            </div>
+        </a>
+        <ul>
+            <li><a href="#">Managed It</a></li>
+            <li><a href="#">Business It</a></li>
+            <li><a href="#">Office 365</a></li>
+            <li><a href="#">Consultancy</a></li>
+            <li><a href="#">Cloud Provider</a></li>
+            <li><a href="#">Data Backup</a></li>
+        </ul>
+    </div>
+
+    <div class="marketing-menu">
+
+        <a href="#">
+            <div class="menu-title">
+            <i class="fas fa-signal market-icon"></i>
+            <p class="small-text">Digital</p>
+            <p class="large-text">Marketing</p>
+            </div>
+        </a>
+        <ul>
+            <li><a href="#">Search (SEO)</a></li>
+            <li><a href="#">Paid (PPC)</a></li>
+            <li><a href="#">Conversion (CRO)</a></li>
+            <li><a href="#">Email</a></li>
+            <li><a href="#">Social Media</a></li>
+            <li><a href="#">Content</a></li>
+        </ul>
+    </div>
+
+    <div class="telecom-menu">
+
+        <a href="#">
+            <div class="menu-title">
+            <i class="fas fa-phone-alt tele-icon"></i>
+            <p class="small-text">Telecoms</p>
+            <p class="large-text">Services</p>
+            </div>
+        </a>
+        <ul>
+            <li><a href="#">Business Mobile</a></li>
+            <li><a href="#">Hosted Voip</a></li>
+            <li><a href="#">Business Voip</a></li>
+            <li><a href="#">Business Broadband</a></li>
+            <li><a href="#">Leased Line</a></li>
+            <li><a href="#">3CX Systems</a></li>
+        </ul>
+    </div>
+
+    <div class="web-menu">
+
+        <a href="#">
+            <div class="menu-title">
+            <i class="fas fa-code web-icon"></i>
+            <p class="small-text">Web</p>
+            <p class="large-text">Design</p>
+            </div>
+        </a>
+        <ul>
+            <li><a href="#">Stylish Websites</a></li>
+            <li><a href="#">Ecommerce Stores</a></li>
+            <li><a href="#">Branding</a></li>
+            <li><a href="#">Apps</a></li>
+            <li><a href="#">Web Hosting</a></li>
+            <li><a href="#">Pay Monthly Websites</a></li>
+        </ul>
+    </div>
+
+    <div class="security-menu">
+
+        <a href="#">
+            <div class="menu-title">
+            <i class="fas fa-shield-alt security-icon"></i>
+            <p class="small-text">Cyber</p>
+            <p class="large-text">Security</p>
+            </div>
+        </a>
+        <ul>
+            <li><a href="#">Assessment</a></li>
+            <li><a href="#">Management</a></li>
+            <li><a href="#">Penetration Testing</a></li>
+            <li><a href="#">Cyber Essentials</a></li>
+            <li><a href="#">PCI/DSS</a></li>
+            <li><a href="#">Hacker Prevention</a></li>
+        </ul>
+    </div>
+
+    <div class="dev-menu">
+
+            <a href="#">
+                <div class="menu-title">
+                <i class="fas fa-graduation-cap developer-icon"></i>
+                <p class="small-text">Developer</p>
+                <p class="large-text">Training</p>
+                </div>
+            </a>
+        <ul>
+            <li><a href="#">Train for a career in tech</a></li>
+            <li><a href="#">Scion scheme frequently asked questions</a></li>
+            <li><a href="#">Scion collaborators</a></li>
+        </ul>
+    </div>
+
+    <div class="services-menu">
+        <a href="#">
+            <div class="menu-title-2">
+            <p>Services</p>
+            </div>
+        </a>
+        <ul>
+            <li><a href="#">Bespoke software</a></li>
+            <li><a href="#">IT support</a></li>
+            <li><a href="#">Digital marketing</a></li>
+            <li><a href="#">Telecoms services</a></li>
+            <li><a href="#">Web design</a></li>
+            <li><a href="#">Cyber security</a></li>
+        </ul>
+    </div>
+
+    <div class="training-menu">
+        <a href="#">
+            <div class="menu-title-2">
+            <p>Training</p>
+            </div>
+        </a>
+        <ul>
+            <li><a href="#">Train for a career in tech</a></li>
+            <li><a href="#">Scs frequently asked questions</a></li>
+            <li><a href="#">Scion collaborators</a></li>
+        </ul>
+    </div>
+
+    <div class="events-menu">
+        <a href="#">
+            <div class="menu-title-2">
+            <p>Events</p>
+            </div>
+        </a>
+        <ul>
+            <li><a href="#">Business automation seminar</a></li>
+        </ul>
+    </div>
+
+    <div class="company-menu">
+        <a href="#">
+            <div class="menu-title-2">
+            <p>Our Company</p>
+            </div>
+        </a>
+        <ul>
+            <li><a href="#">Our Culture</a></li>
+            <li><a href="#">Our Team</a></li>
+            <li><a href="#">Our Careers</a></li>
+            <li><a href="#">Our Benefits</a></li>
+            <li><a href="#">It support Great Yarmouth</a></li>
+        </ul>
+    </div>
+
+    <div class="work-menu">
+        <a href="#">
+            <div class="menu-title-2">
+            <p>Our Work</p>
+            </div>
+        </a>
+        <ul>
+            <li><a href="#">Case Studies</a></li>
+        </ul>
+    </div>
+
+    <div class="knowledge-menu">
+        <a href="#">
+            <div class="menu-title-2">
+            <p>Our Knowledge</p>
+            </div>
+        </a>
+        <ul>
+            <li><a href="#">Guides</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Insights</a></li>
+        </ul>
+    </div>
+
+    <div class="covid-menu">
+        <a href="#">
+            <div class="menu-title-2">
+            <p>Covid Risk Assessment</p>
+            </div>
+        </a>
+    </div>
+
+    <div class="contact-menu">
+        <a href="#">
+            <div class="menu-title-2">
+            <p>Contact us</p>
+            </div>
+        </a>
+        <ul>
+            <li><a href="#">Cambridge Office</a></li>
+            <li><a href="#">Wymondham Office</a></li>
+            <li><a href="#">Great Yarmouth Office</a></li>
+        </ul>
+    </div>
+
+    <!-- <div class="empty-menu">
+        <a href="#"></a>
+    </div>
+
+    <div class="empty-menu">
+        <a href="#"></a>
+    </div> -->
+
+    <div class="support-menu">
+        <a href="#">
+            <div class="menu-title-2">
+            <p>Support</p>
+            </div>
+        </a>
+    </div>
+
+</div>
+
+<div class="entire-page">
+    <div class="darken-page"></div>
+    <!-- Header/nav bar -->
+    <header class="sticky">
+        <div class="header">
+            <div class="center">
+                <a href="#"><img src="Images/f-logo.png" alt="Netmatters logo"></a>
+                <a href="tel:#" class="phone small-phone"><i class="fas fa-phone-alt"></i></a>
+                <a href="#"><div class="support"><i class="fas fa-mouse"></i>Support</div></a>
+                <a href="#"><div class="contact"><i class="far fa-paper-plane"></i>Contact</div></a>
+                <div class="menu small-position">
+                    <span class="menu-icon"></span>
+                    <span class="menu-icon-2"></span>
+                    <span class="menu-icon-3"></span>
+                    <p>Menu</p>
+                </div>
+                <div class="header-form">
+                    <form class="small-form">
+                    <input type="text" placeholder="Search..." name="search">
+                    <button type="submit" class="search"><i class="fas fa-search"></i></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <!-- Secondary nav bar -->
+
+        <div class="background-fix-5">
+            <div class="nav-bar">
+
+                <div class="bespoke-nav">
+                    <a href="#">
+                        <i class="fas fa-th bespoke-icon"></i>
+                        <p class="small-text">Bespoke</p>
+                        <p class="large-text">Software</p>
+                    </a>
+
+                    <div class="sub-nav">
+                        <div class="bespoke-nav-sub">
+                            <div class="nav-sub-links">
+                                <div class="background-fix-nav">
+                                    <p class="link-dash"><a href="#">Workflow</a></p>
+                                    <p class="link-dash"><a href="#">Automation</a></p>
+                                    <p class="link-dash"><a href="#">Integration</a></p>
+                                    <p class="link-dash"><a href="#">Database</a></p>
+                                    <p class="link-dash"><a href="#">Sharepoint</a></p>
+                                    <p class="link-dash"><a href="#">Management</a></p>
+                                    <p><a href="#">Navision</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="it-nav">
+                    <a href="#">
+                        <i class="fas fa-desktop it-icon"></i>
+                        <p class="small-text">IT</p>
+                        <p class="large-text">Support</p>
+                    </a>
+
+                    <div class="sub-nav">
+                        <div class="it-nav-sub">
+                            <div class="nav-sub-links">
+                                <div class="background-fix-nav">
+                                    <p class="link-dash"><a href="#">Managed IT</a></p>
+                                    <p class="link-dash"><a href="#">Business IT</a></p>
+                                    <p class="link-dash"><a href="#">Office 365</a></p>
+                                    <p class="link-dash"><a href="#">Consultancy</a></p>
+                                    <p class="link-dash"><a href="#">Cloud Provider</a></p>
+                                    <p><a href="#">Data Backup</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="marketing-nav">
+                    <a href="#">
+                        <i class="fas fa-signal market-icon"></i>
+                        <p class="small-text">Digital</p>
+                        <p class="large-text">Marketing</p>
+                    </a>
+
+                    <div class="sub-nav">
+                        <div class="market-nav-sub">
+                            <div class="nav-sub-links">
+                                <div class="background-fix-nav">
+                                    <p class="link-dash"><a href="#">Search (SEO)</a></p>
+                                    <p class="link-dash"><a href="#">Paid (PPC)</a></p>
+                                    <p class="link-dash"><a href="#">Conversion (CRO)</a></p>
+                                    <p class="link-dash"><a href="#">Email</a></p>
+                                    <p class="link-dash"><a href="#">Social Media</a></p>
+                                    <p><a href="#">Content</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="telecom-nav">
+                    <a href="#">
+                        <i class="fas fa-phone-alt tele-icon"></i>
+                        <p class="small-text">Telecoms</p>
+                        <p class="large-text">Services</p>
+                    </a>
+
+                    <div class="sub-nav">
+                        <div class="tele-nav-sub">
+                            <div class="nav-sub-links">
+                                <div class="background-fix-nav">
+                                    <p class="link-dash"><a href="#">Business Mobile</a></p>
+                                    <p class="link-dash"><a href="#">Hosted Voip</a></p>
+                                    <p class="link-dash"><a href="#">Business Voip</a></p>
+                                    <p class="link-dash"><a href="#">Business Broadband</a></p>
+                                    <p class="link-dash"><a href="#">Leased Line</a></p>
+                                    <p><a href="#">3CX Systems</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="web-nav">
+                    <a href="#">
+                        <i class="fas fa-code web-icon"></i>
+                        <p class="small-text">Web</p>
+                        <p class="large-text">Design</p>
+                    </a>
+
+                    <div class="sub-nav">
+                        <div class="web-nav-sub">
+                            <div class="nav-sub-links">
+                                <div class="background-fix-nav">
+                                    <p class="link-dash"><a href="#">Stylish Websites</a></p>
+                                    <p class="link-dash"><a href="#">Ecommerce Stores</a></p>
+                                    <p class="link-dash"><a href="#">Branding</a></p>
+                                    <p class="link-dash"><a href="#">Apps</a></p>
+                                    <p class="link-dash"><a href="#">Web Hosting</a></p>
+                                    <p><a href="#">Pay Monthly Websites</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="security-nav">
+                    <a href="#">
+                        <i class="fas fa-shield-alt security-icon"></i>
+                        <p class="small-text">Cyber</p>
+                        <p class="large-text">Security</p>
+                    </a>
+
+                    <div class="sub-nav">
+                        <div class="security-nav-sub">
+                            <div class="nav-sub-links">
+                                <div class="background-fix-nav">
+                                    <p class="link-dash"><a href="#">Assessment</a></p>
+                                    <p class="link-dash"><a href="#">Management</a></p>
+                                    <p class="link-dash"><a href="#">Penetration Testing</a></p>
+                                    <p class="link-dash"><a href="#">Cyber Essential</a></p>
+                                    <p class="link-dash"><a href="#">PCI/DSS</a></p>
+                                    <p><a href="#">Hacker Prevention</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="developer-nav">
+                    <a href="#">
+                        <i class="fas fa-graduation-cap developer-icon"></i>
+                        <p class="small-text">Developer</p>
+                        <p class="large-text">Training</p>
+                    </a>
+
+                    <div class="sub-nav">
+                        <div class="dev-nav-sub">
+                            <div class="nav-sub-links">
+                                <div class="background-fix-nav">
+                                    <p class="link-dash"><a href="#">Train For A Career In Tech</a></p>
+                                    <p class="link-dash"><a href="#">Scion Scheme Frequently Asked Questions</a></p>
+                                    <p><a href="#">Scion Collaborators</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </header>
